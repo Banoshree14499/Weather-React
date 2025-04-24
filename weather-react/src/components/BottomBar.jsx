1b1e4de6e3c61613626a9009a5370ac066a70ab5
Banoshree14499 <banoshreebose14499@gmail.com>
2025-04-24 13:13:46 +0530

    Background Updated

---
+++ weather-react/src/components/BottomBar.jsx
@@ -0,0 +1,11 @@
+const BottomBar=()=>{
+    return(
+        <div className="bottom-bar">
+            <div className="bottom-bar-content">
+                
+            </div>
+        </div>
+    )
+}
+
+export default BottomBar;
